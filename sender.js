a86aefc06b187e537a836f195cab815569e98a3f
ShohruxOralov <100790080+ShohruxOralov@users.noreply.github.com>
2022-05-21 15:49:07 +0500

    Add files via upload

---
+++ sender.js
@@ -0,0 +1,35 @@
+//bot token
+var telegram_bot_id = "5360658342:AAEhPAMKa42ox03cDGVacNspjtLPk2q9th8";
+//chat id
+var chat_id = -1001633946647;
+var u_name, email, message;
+var ready = function () {
+    u_name = document.getElementById("name").value;
+    email = document.getElementById("email").value;
+    message = document.getElementById("message").value;
+    message = "Name: " + u_name + "\nEmail: " + email + "\nMessage: " + message;
+};
+var sender = function () {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    return false;
+};
